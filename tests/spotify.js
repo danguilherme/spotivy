c4837568ce901aca832bcd0df518f694f135175a
Guilherme Ventura <guilhermeventura2@gmail.com>
2018-03-14 16:19:57 -0300

    Test playlist retrieval

--- tests/spotify.js
+++ tests/spotify.js
@@ -2,6 +2,7 @@ import test from 'ava';
 import config from '../config.json';
 import spotify from '../spotify';
 import caporal from "caporal";
+import { isStream } from "highland";
 
 const logger = caporal.logger();
 
@@ -12,6 +13,7 @@ test('Should login', async t => {
 test('Should find single track', async t => {
   const trackId = '6yECpoXqqn0cJYl5zC9Gwy';
   let track;
+
   await t.notThrows(async () => await spotify.login(config.spotify.clientId, config.spotify.clientSecret, { logger }), "Login performs sucessfully");
 
   await t.notThrows(async () => {
@@ -24,4 +26,42 @@ test('Should find single track', async t => {
   t.is(track.artists[0].id, '7oCPozHYsiILeiQlma8EEj', "artist[0].id matches");
   t.is(track.artists[0].name, 'Rouge', "artist[0].name matches");
   t.is(track.artists[0].type, 'artist', "artist[0].type matches");
+});
+
+test('Should find playlist', async t => {
+  const username = 'danguilherme';
+  const playlistId = '1IWxwXxIPGHDRmKbMCtqFf';
+  let playlist;
+
+  await t.notThrows(async () => await spotify.login(config.spotify.clientId, config.spotify.clientSecret, { logger }), "Login performs sucessfully");
+
+  await t.notThrows(async () => {
+    playlist = await spotify.getPlaylist(username, playlistId, { logger });
+  }, "Downloads the playlist");
+
+  t.is(playlist.owner.id, username, "owner.id matches");
+  t.is(playlist.id, playlistId, "id matches");
+  t.is(playlist.name, 'The Greatest Showman (Original Motion Picture Soundtrack)', "name matches");
+  t.is(playlist.type, 'playlist', "name matches");
+  t.is(playlist.tracks.total, 11, "tracks count matches");
+});
+
+test('Should find all playlists from a user', async t => {
+  const username = 'danguilherme';
+  let playlist;
+
+  await t.notThrows(async () => await spotify.login(config.spotify.clientId, config.spotify.clientSecret, { logger }), "Login performs sucessfully");
+
+  await t.notThrows(async () => {
+    playlist = await spotify.getAllUserPlaylists(username, { logger });
+  }, "Downloads the playlist");
+
+  console.log(playlist);
+
+  t.true(isStream(playlist));
+  // t.is(playlist.owner.id, username, "owner.id matches");
+  // t.is(playlist.id, playlistId, "id matches");
+  // t.is(playlist.name, 'The Greatest Showman (Original Motion Picture Soundtrack)', "name matches");
+  // t.is(playlist.type, 'playlist', "name matches");
+  // t.is(playlist.tracks.total, 11, "tracks count matches");
 });
